07679716a6334c2dd0089d6a485ddc11514e5a0f
paffare <anton.lundborg@gmail.com>
2015-06-08 22:03:25 +0200

    Map activity moved to fragment and is now clickable in the drawer

--- app/src/main/java/com/lth/antonlundborg/antonsprojekt/MainActivity.java
+++ app/src/main/java/com/lth/antonlundborg/antonsprojekt/MainActivity.java
@@ -1,18 +1,65 @@
 package com.lth.antonlundborg.antonsprojekt;
 
-import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 
-public class MainActivity extends ActionBarActivity {
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private Drawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigationDrawer();
+
+        fragmentManager = getSupportFragmentManager();
+
+
+    }
+
+    private void setupNavigationDrawer() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        Drawable drawable = getResources().getDrawable(R.drawable.drawer_background);
+        AccountHeader accountHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(drawable)
+                .build();
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withActionBarDrawerToggleAnimated(true)
+                .withSliderBackgroundColorRes(R.color.slider_background)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_home),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_charts),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_wind)
+                )
+                .withOnDrawerItemClickListener(new MyOnDrawerItemClickListener())
+                .withAccountHeader(accountHeader)
+                .build();
     }
 
     @Override
@@ -36,12 +83,28 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void onClick(View view){
-        if(view.getId() == R.id.smhiButton){
-            startActivity(new Intent(this, MapsActivity.class));
-        }
-        if(view.getId() == R.id.waveChartButton){
-            startActivity(new Intent(this, ChartActivity.class));
+
+    public void onClick(View view) {
+
+    }
+
+
+    private class MyOnDrawerItemClickListener implements Drawer.OnDrawerItemClickListener{
+
+        @Override
+        public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+            drawer.closeDrawer();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            Fragment fragment;
+            if(i == 3) {
+                fragment = MapFragment.newInstance();
+                transaction.add(R.id.fragment_container, fragment);
+                transaction.commit();
+            }
+            if(i == 1){
+
+            }
+            return true;
         }
     }
 }
